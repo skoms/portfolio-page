4e4ffc4e6d0465c3e1ec08f2f32296461a8f6644
Skoms <andrew.m.s.97@hotmail.com>
2021-11-20 17:56:22 +0100

    feat(about): add feature

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,8 +6,18 @@ export const About: React.FC = () => {
 		<div className={styles.container}>
 			<h2>Hello there!</h2>
 			<p>
-				I am Andreas Myklebust Skomsøy, an aspiring Web Developer from Norway!
+				I am Andreas Myklebust Skomsøy, an aspiring Web Developer from Norway
+				<img
+					src='/images/norway-flag.png'
+					alt='norwegian-flag'
+					className={styles.flag}
+				/>
 			</p>
+			<p>
+				I love learning new things, hiking in the woods and mountains, and
+				exploring.
+			</p>
+			<p>I love languages (both Human and Computer), history and trivia.</p>
 		</div>
 	)
 }
